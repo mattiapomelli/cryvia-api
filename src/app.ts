5deb064fd1074d21864764c61514b9129d159cf8
Mattia Pomelli <mattiapomelli@gmail.com>
2022-05-07 10:14:58 +0200

    Fix controllers not being load

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Express } from 'express'
 import dotenv from 'dotenv'
-// import glob from 'glob'
+import glob from 'glob'
 import { WebSocketServer } from 'ws'
 import http from 'http'
 import handleSocketConnection from 'sockets/quiz'
 
-// import controllers from './utils/controllers'
+import controllers from './utils/controllers'
 
 dotenv.config()
 
@@ -19,16 +19,16 @@ wss.on('connection', handleSocketConnection)
 
 app.use(express.json())
 
-// controllers.init(app)
+controllers.init(app)
 
 // Load controllers
 
-// const files = glob.sync('src/controllers/**/*.ts')
+const files = glob.sync('src/controllers/**/*.ts')
 
-// for (const filePath of files) {
-//   const path = '.' + filePath.replace('src', '').split('.')[0]
-//   import(path)
-// }
+for (const filePath of files) {
+  const path = '.' + filePath.replace('src', '').split('.')[0]
+  import(path)
+}
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`)
