8de80ceb272b3f0679da623a66e8f587c7e31fb2
Mattia Pomelli <mattiapomelli@gmail.com>
2022-06-25 19:03:57 +0200

    Add function to get contract and signer

--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -1,8 +1,9 @@
-import { ethers } from 'ethers'
+import { ethers, Signer } from 'ethers'
 
-import { QUIZ_CONTRACT_ADDRESS } from 'constants/addresses'
+import { QUIZ_CONTRACT_ADDRESS, TOKEN_ADDRESS } from 'constants/addresses'
 import QuizContractAbi from '@abis/contracts/Quiz.json'
-import { Quiz } from '@abis/types'
+import ERC20Abi from '@abis/contracts/ERC20.json'
+import { Quiz, ERC20 } from '@abis/types'
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER_URL)
 
@@ -10,12 +11,23 @@ if (!process.env.OWNER_PRIVATE_KEY) {
   throw Error('Environment variable OWNER_PRIVATE_KEY must be defined')
 }
 
-const signer = new ethers.Wallet(process.env.OWNER_PRIVATE_KEY, provider)
+export const getSigner = (privateKey: string) =>
+  new ethers.Wallet(privateKey, provider)
 
-export async function getQuizContract() {
+const owner = getSigner(process.env.OWNER_PRIVATE_KEY)
+
+export async function getQuizContract(signer?: Signer) {
   return new ethers.Contract(
     QUIZ_CONTRACT_ADDRESS,
     QuizContractAbi.abi,
-    signer,
+    signer || owner,
   ) as Quiz
+}
+
+export async function getTokenContract(signer?: Signer) {
+  return new ethers.Contract(
+    TOKEN_ADDRESS,
+    ERC20Abi.abi,
+    signer || owner,
+  ) as ERC20
 }
